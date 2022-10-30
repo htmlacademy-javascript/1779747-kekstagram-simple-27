c6061dce7896feb3cebaa12a385f8c88cba7ddcf
poluectov <poluectov@list.ru>
2022-10-30 20:33:12 +0300

    Update main.js
    добавил генерацию 25 обьектов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-function getRandomRange(min, max) {
+
+const PHOTOS = [
+  "пляж",
+  "указатель",
+  "бухта",
+  "девушка",
+  "суп",
+  "машина",
+  "клубника",
+  "сок",
+  "самолет",
+  "обувь",
+  "пляж",
+  "ауди",
+  "сельдерей",
+  "котенок",
+  "космо-тапки",
+  "облака",
+  "хор",
+  "коломбо",
+  "тапки",
+  "пальмы",
+  "курица",
+  "закат",
+  "краб",
+  "концерт",
+  "бегемот",
+];
+const getRandomRange = (min, max) => {
   if (min < 0 || max <0){
     return NaN;
   }
@@ -10,6 +38,18 @@ function getRandomRange(min, max) {
 }
 
 
- function getStringLength(valueString, maxLength) {
-   return valueString.length <= maxLength;
- }
+function getStringLength(valueString, maxLength) {
+  return valueString.length <= maxLength;
+}
+const getObjectPhoto = function (indexPhoto) {
+  return {
+    id: indexPhoto + 1,
+    url: `photos/${indexPhoto + 1}.jpg`,
+    description: PHOTOS[indexPhoto],
+    likes: getRandomRange(15, 200),
+    comments: getRandomRange(0, 200),
+  };
+};
+
+const generateArray = Array.from({length: PHOTOS.length}, (v, i) =>getObjectPhoto(i));
+
